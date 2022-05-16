754eff337e2e6d2776f50dd7020d7e466b7d40c2
prvn-codes <iamprvn.s@gmail.com>
2022-05-16 13:14:24 +0530

    First stage - CA2 - Rocket env

---
+++ rocket.py
@@ -0,0 +1,287 @@
+import numpy as np
+import random
+import simplejson
+
+
+class Rocket(object):
+    """
+    Rocekt and environment.
+    The rocket is simplified into a rigid body model with a thin rod,
+    considering acceleration and angular acceleration and air resistance
+    proportional to velocity.
+
+    There are two tasks: hover and landing
+    Their reward functions are straight forward and simple.
+
+    For the hover tasks: the step-reward is given based on two factors
+    1) the distance between the rocket and the predefined target point
+    2) the angle of the rocket body (the rocket should stay as upright as possible)
+
+    For the landing task: the step-reward is given based on three factors:
+    1) the distance between the rocket and the predefined landing point.
+    2) the angle of the rocket body (the rocket should stay as upright as possible)
+    3) Speed and angle at the moment of contact with the ground, when the touching-speed
+    are smaller than a safe threshold and the angle is close to 90 degrees (upright),
+    we see it as a successful landing.
+
+    """
+
+    def __init__(self, max_steps, task='hover', rocket_type='falcon'):
+
+        self.task = task
+        self.rocket_type = rocket_type
+
+        self.g = 9.8
+        self.H = 50  # rocket height (meters)
+        self.I = 1/12*self.H*self.H  # Moment of inertia
+        self.dt = 0.05
+
+        self.world_x_min = -300  # meters
+        self.world_x_max = 300
+        self.world_y_min = -30
+        self.world_y_max = 570
+
+        # target point
+        if self.task == 'hover':
+            self.target_x, self.target_y, self.target_r = 0, 200, 50
+        elif self.task == 'landing':
+            self.target_x, self.target_y, self.target_r = 0, self.H/2.0, 50
+
+        self.already_landing = False
+        self.already_crash = False
+        self.max_steps = max_steps
+
+        self.step_id = 0
+
+        self.state = self.create_random_state()
+        self.action_table = self.create_action_table()
+
+        self.state_dims = 8
+        self.action_dims = len(self.action_table)
+
+        self.state_buffer = []
+
+    def reset(self, state_dict=None):
+
+        if state_dict is None:
+            self.state = self.create_random_state()
+        else:
+            self.state = state_dict
+
+        self.state_buffer = []
+        self.step_id = 0
+        self.already_landing = False
+        return self.flatten(self.state)
+
+    def create_action_table(self):
+        f0 = 0.2 * self.g  # thrust
+        f1 = 1.0 * self.g
+        f2 = 2 * self.g
+        vphi0 = 0  # Nozzle angular velocity
+        vphi1 = 30 / 180 * np.pi
+        vphi2 = -30 / 180 * np.pi
+
+        action_table = [[f0, vphi0], [f0, vphi1], [f0, vphi2],
+                        [f1, vphi0], [f1, vphi1], [f1, vphi2],
+                        [f2, vphi0], [f2, vphi1], [f2, vphi2]
+                        ]
+        return action_table
+
+    def get_random_action(self):
+        return random.randint(0, len(self.action_table)-1)
+
+    def create_random_state(self):
+
+        # predefined locations
+        x_range = self.world_x_max - self.world_x_min
+        y_range = self.world_y_max - self.world_y_min
+        xc = (self.world_x_max + self.world_x_min) / 2.0
+        yc = (self.world_y_max + self.world_y_min) / 2.0
+
+        if self.task == 'landing':
+            x = random.uniform(xc - x_range / 4.0, xc + x_range / 4.0)
+            y = yc + 0.4*y_range
+            if x <= 0:
+                theta = -85 / 180 * np.pi
+            else:
+                theta = 85 / 180 * np.pi
+            vy = -50
+
+        if self.task == 'hover':
+            x = xc
+            y = yc + 0.2 * y_range
+            theta = random.uniform(-45, 45) / 180 * np.pi
+            vy = -10
+
+        state = {
+            'x': x, 'y': y, 'vx': 0, 'vy': vy,
+            'theta': theta, 'vtheta': 0,
+            'phi': 0, 'f': 0,
+            't': 0, 'a_': 0
+        }
+
+        return state
+
+    def check_crash(self, state):
+        if self.task == 'hover':
+            x, y = state['x'], state['y']
+            theta = state['theta']
+            crash = False
+            if y <= self.H / 2.0:
+                crash = True
+            if y >= self.world_y_max - self.H / 2.0:
+                crash = True
+            return crash
+
+        elif self.task == 'landing':
+            x, y = state['x'], state['y']
+            vx, vy = state['vx'], state['vy']
+            theta = state['theta']
+            vtheta = state['vtheta']
+            v = (vx**2 + vy**2)**0.5
+
+            crash = False
+            if y >= self.world_y_max - self.H / 2.0:
+                crash = True
+            if y <= 0 + self.H / 2.0 and v >= 15.0:
+                crash = True
+            if y <= 0 + self.H / 2.0 and abs(x) >= self.target_r:
+                crash = True
+            if y <= 0 + self.H / 2.0 and abs(theta) >= 10/180*np.pi:
+                crash = True
+            if y <= 0 + self.H / 2.0 and abs(vtheta) >= 10/180*np.pi:
+                crash = True
+            return crash
+
+    def check_landing_success(self, state):
+        if self.task == 'hover':
+            return False
+        elif self.task == 'landing':
+            x, y = state['x'], state['y']
+            vx, vy = state['vx'], state['vy']
+            theta = state['theta']
+            vtheta = state['vtheta']
+            v = (vx**2 + vy**2)**0.5
+            return True if y <= 0 + self.H / 2.0 and v < 15.0 and abs(x) < self.target_r \
+                           and abs(theta) < 10/180*np.pi and abs(vtheta) < 10/180*np.pi else False
+
+    def calculate_reward(self, state):
+
+        x_range = self.world_x_max - self.world_x_min
+        y_range = self.world_y_max - self.world_y_min
+
+        # dist between agent and target point
+        dist_x = abs(state['x'] - self.target_x)
+        dist_y = abs(state['y'] - self.target_y)
+        dist_norm = dist_x / x_range + dist_y / y_range
+
+        dist_reward = 0.1*(1.0 - dist_norm)
+
+        if abs(state['theta']) <= np.pi / 6.0:
+            pose_reward = 0.1
+        else:
+            pose_reward = abs(state['theta']) / (0.5*np.pi)
+            pose_reward = 0.1 * (1.0 - pose_reward)
+
+        reward = dist_reward + pose_reward
+
+        # hit target
+        if self.task == 'hover' and (dist_x**2 + dist_y**2)**0.5 <= 2*self.target_r:
+            reward = 0.25
+        # hit target
+        if self.task == 'hover' and (dist_x**2 + dist_y**2)**0.5 <= 1*self.target_r:
+            reward = 0.5
+        if self.task == 'hover' and abs(state['theta']) > 90 / 180 * np.pi:
+            reward = 0
+
+        v = (state['vx'] ** 2 + state['vy'] ** 2) ** 0.5
+        if self.task == 'landing' and self.already_crash:
+            reward = (reward + 5*np.exp(-1*v/10.)) * \
+                (self.max_steps - self.step_id)
+        if self.task == 'landing' and self.already_landing:
+            reward = (1.0 + 5*np.exp(-1*v/10.))*(self.max_steps - self.step_id)
+
+        return reward
+
+    def step(self, action):
+
+        x, y, vx, vy = self.state['x'], self.state['y'], self.state['vx'], self.state['vy']
+        theta, vtheta = self.state['theta'], self.state['vtheta']
+        phi = self.state['phi']
+
+        f, vphi = self.action_table[action]
+
+        ft, fr = -f*np.sin(phi), f*np.cos(phi)
+        fx = ft*np.cos(theta) - fr*np.sin(theta)
+        fy = ft*np.sin(theta) + fr*np.cos(theta)
+
+        # suppose after 125 m free fall, then air resistance = mg
+        rho = 1 / (125/(self.g/2.0))**0.5
+        ax, ay = fx-rho*vx, fy-self.g-rho*vy
+        atheta = ft*self.H/2 / self.I
+
+        # update agent
+        if self.already_landing:
+            vx, vy, ax, ay, theta, vtheta, atheta = 0, 0, 0, 0, 0, 0, 0
+            phi, f = 0, 0
+            action = 0
+
+        self.step_id += 1
+        x_new = x + vx*self.dt + 0.5 * ax * (self.dt**2)
+        y_new = y + vy*self.dt + 0.5 * ay * (self.dt**2)
+        vx_new, vy_new = vx + ax * self.dt, vy + ay * self.dt
+        theta_new = theta + vtheta*self.dt + 0.5 * atheta * (self.dt**2)
+        vtheta_new = vtheta + atheta * self.dt
+        phi = phi + self.dt*vphi
+
+        phi = max(phi, -20/180*3.1415926)
+        phi = min(phi, 20/180*3.1415926)
+
+        self.state = {
+            'x': x_new, 'y': y_new, 'vx': vx_new, 'vy': vy_new,
+            'theta': theta_new, 'vtheta': vtheta_new,
+            'phi': phi, 'f': f,
+            't': self.step_id, 'action_': action
+        }
+        self.state_buffer.append(self.state)
+
+        self.already_landing = self.check_landing_success(self.state)
+        self.already_crash = self.check_crash(self.state)
+        reward = self.calculate_reward(self.state)
+
+        if self.already_crash or self.already_landing:
+            done = True
+        else:
+            done = False
+
+        return self.flatten(self.state), reward, done, None
+
+    def flatten(self, state):
+        x = [state['x'], state['y'], state['vx'], state['vy'],
+             state['theta'], state['vtheta'], state['t'],
+             state['phi']]
+        return np.array(x, dtype=np.float32)/100.
+
+
+if __name__ == "__main__":
+
+    file = open("inference.txt", "w")
+
+    rocket = Rocket(max_steps=50)
+
+    file.write("\n\nAction Dimension : ")
+    file.write(str(rocket.action_dims))
+
+    file.write("\n\nActions\n")
+    simplejson.dump(rocket.action_table, file)
+
+    file.write("\n\nA Random State : ")
+    simplejson.dump(rocket.state, file)
+
+    file.write("\n\nState Dimension : ")
+    file.write(str(rocket.state_dims))
+
+    file.write("\n\nA Random Action : ")
+    simplejson.dump(rocket.get_random_action(), file)
+
+    file.close()
